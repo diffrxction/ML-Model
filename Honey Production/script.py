9d39835e06a4192b7efb4c08da63110077f89cee
Aryan <aryandiffract@gmail.com>
2020-10-09 08:43:42 +0530

    Update script.py

--- Honey Production/script.py
+++ Honey Production/script.py
@@ -5,36 +5,50 @@ import numpy as np
 from sklearn import linear_model
 
 df = pd.read_csv("https://content.codecademy.com/programs/data-science-path/linear_regression/honeyproduction.csv")
+
 #1
 print(df.head())
+
 #2
 prod_per_year = df.groupby('year').totalprod.mean().reset_index()
+print(prod_per_year)
+
 #3
-X = df["year"]
+X = prod_per_year["year"]
 X = X.values.reshape(-1,1)
+
 #4
-y = df["totalprod"]
+y = prod_per_year["totalprod"]
+
 #5
-#plt.scatter(X, y)
-#plt.show()
+plt.scatter(X, y)
+plt.show()
+
 #6
 regr = linear_model.LinearRegression()
+
 #7
 regr.fit(X,y)
+
 #8
 print(regr.coef_[0])
+print(regr.intercept_)
+
 #9
 y_predict = regr.predict(X)
+print(y_predict)
+
 #10
 plt.plot(X, y_predict)
 plt.show()
+
 #11
 X_future = np.array(range(2013, 2050))
-
 X_future = X_future.reshape(-1,1)
 
 #12
-future_predict = np.array(regr.predict(X_future))
+future_predict = regr.predict(X_future)
+
 #13
 plt.plot(X_future, future_predict)
 plt.show()
