cf41279542a8b0fd61010f0fa8174e38155811b1
Aryan <65293175+diffrxction@users.noreply.github.com>
2020-10-23 15:32:25 +0530

    Update script.py

--- K-nearest Neighbors/Breast Cancer Classifier/script.py
+++ K-nearest Neighbors/Breast Cancer Classifier/script.py
@@ -11,8 +11,6 @@ print(breast_cancer_data.feature_names)
 print(breast_cancer_data.target)
 print(breast_cancer_data.target_names)
 
-train_test_split(breast_cancer_data.data, breast_cancer_data.target, test_size = 0.2, random_state = 100)
-
 training_data, validation_data, training_labels, validation_labels = train_test_split(breast_cancer_data.data, breast_cancer_data.target, test_size = 0.2, random_state = 60)
 
 print(len(training_data))
